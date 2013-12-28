bbcca79df55bec81d916b7a13b27265e27ad1d99
Douglas Campos <qmx@qmx.me>
2013-12-28 01:06:25 -0200

    generate bower.json from package.json

--- build.js
+++ build.js
@@ -1,7 +1,16 @@
 var bundle = require('browserify')();
 var fs = require('fs');
 var uglify = require('uglify-js');
+var pack = require('./package.json');
 
+var bower = {
+    name: pack.name,
+    version: pack.version,
+    main: pack.main,
+    dependencies: pack.dependencies
+};
+
+fs.writeFileSync('bower.json', JSON.stringify(bower, null, 2));
 
 bundle.add('./human-view');
 bundle.bundle({standalone: 'HumanView'}, function (err, source) {
