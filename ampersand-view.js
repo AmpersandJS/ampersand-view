61cfeedc0adf0a354cb129100db738e302d1294d
Kamil Ogórek <kamil.ogorek@gmail.com>
2015-05-27 15:15:54 +0200

    Updated error message for multiple template roots

--- ampersand-view.js
+++ ampersand-view.js
@@ -295,7 +295,7 @@ _.extend(View.prototype, {
         if (_.isString(newDom)) newDom = domify(newDom);
         var parent = this.el && this.el.parentNode;
         if (parent) parent.replaceChild(newDom, this.el);
-        if (newDom.nodeName === '#document-fragment') throw new Error('Views can only have one root element.');
+        if (newDom.nodeName === '#document-fragment') throw new Error('Views can only have one root element, including comment nodes.');
         this.el = newDom;
         return this;
     },
